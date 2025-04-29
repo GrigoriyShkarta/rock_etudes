87e432f9a91df53433ec1a99821df2a80f0e2667
hryhorii_shkarta <grishkoshkarta1@gmail.com>
2025-04-29 15:46:20 +0300

    fixed styles

--- src/sections/main/index.tsx
+++ src/sections/main/index.tsx
@@ -1,9 +1,9 @@
-import { FC } from "react";
-import logo from "../../assets/img/logo.webp";
+import { FC } from 'react'
+import logo from '../../assets/img/logo.webp'
 // import main from "../../assets/img/main.webp";
-import rock_etudes from "../../assets/img/rock_etudes.png";
-import Section from "../../section";
-import s from "./style.module.scss";
+import rock_etudes from '../../assets/img/rock_etudes.png'
+import Section from '../../section'
+import s from './style.module.scss'
 
 const Main: FC = () => (
 	<div className={s.section}>
@@ -13,18 +13,18 @@ const Main: FC = () => (
 					{/* <img className={s.img} src={main} alt="rock etudes" /> */}
 					<div className={s.desctop}>
 						<div className={s.top}>
-							<img className={s.logo} src={logo} alt="ys vocal coach" />
+							<img className={s.logo} src={logo} alt='ys vocal coach' />
 							<p className={s.name}>YS VOCAL COACH</p>
 						</div>
 						<div className={s.bottom}>
-							<img src={rock_etudes} alt="rock etudes" />
+							<img src={rock_etudes} className={s.banner} alt='rock etudes' />
 							<p className={s.text}>
 								13 рок-вправ для свідомого розвитку твоїх вокальних здібностей
 							</p>
 						</div>
 					</div>
-					<button className={s.button} type={"button"}>
-						<a href="https://t.me/ys_department_of_care_bot?start=6742f1d3e01c8aca7d035e5f">
+					<button className={s.button} type={'button'}>
+						<a href='https://t.me/ys_department_of_care_bot?start=6742f1d3e01c8aca7d035e5f'>
 							Придбати
 						</a>
 					</button>
@@ -33,6 +33,6 @@ const Main: FC = () => (
 			}
 		/>
 	</div>
-);
+)
 
-export default Main;
+export default Main
